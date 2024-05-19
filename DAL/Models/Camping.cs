63c6997e930a1f9c697525bf8df6e5d02fc84e8f
Miguel Saez Perez <misape575@gmail.com>
2024-05-19 15:48:51 +0200

    Creado DAL + 2 modelos

---
+++ DAL/Models/Camping.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace DAL.Models
+{
+    public class Camping
+    {
+        public string Id = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public Blob Portrait {  get; set; }
+
+        [Required]
+        public float CoordX { get; set; }
+
+        [Required]
+        public float CoordY { get; set; }
+
+    }
+}
---
+++ DAL/Models/Service.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace DAL.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public Blob Icon { get; set; }
+    }
+}
