ee86003e948f501ce6ece7e02e64556f8b9c5b44
Miguel Saez Perez <misape575@gmail.com>
2024-05-19 16:52:49 +0200

    Plantilla endpoints de camping

---
+++ API/Controllers/CampingController.cs
@@ -0,0 +1,39 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class CampingController : ControllerBase
+    {
+        private readonly ILogger<CampingController> _logger;
+
+        public CampingController(ILogger<CampingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Campings")]
+        public IEnumerable<Camping> GetCampingList()
+        {
+            
+        }
+
+        [HttpGet("Campings/{id}")]
+        public Camping GetCamping(string id) 
+        {
+            return new Camping();
+        }
+
+        [HttpPost("Campings/Post")]
+        public async Task UploadCamping(Camping camping)
+        {
+
+        }
+
+        [HttpDelete("Campings/Delete/{id}")]
+        public async Task DeleteCamping(string id)
+        {
+
+        }
+    }
+}
