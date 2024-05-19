20319dd3c2f0f9fa2fe79cd460f19c68c98969e0
Miguel Saez Perez <misape575@gmail.com>
2024-05-19 19:27:40 +0200

    Añadido DBContext (solo camping)

---
+++ DAL/DBContext.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext() { }
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+
+        public virtual DbSet<Camping> Campings { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Camping>(entity =>
+            {
+                entity.ToTable("Campings");
+                entity.Property(c => c.Id).HasColumnName("Id");
+                entity.Property(c => c.Name).HasMaxLength(20);
+                entity.Property(c => c.Description).HasMaxLength(100);
+                entity.Property(c => c.Portrait).IsUnicode(false);
+                entity.Property(c => c.CoordX).IsUnicode(false);
+                entity.Property(c => c.CoordY).IsUnicode(false);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
