fe86ac1e1ab08ab60edafd9f25ee4414fe597cbb
Miguel Saez Perez <misape575@gmail.com>
2024-05-23 20:35:09 +0200

    Mensaje de login fallido

--- CampingWebAssembly/Pages/Index.razor.cs
+++ CampingWebAssembly/Pages/Index.razor.cs
@@ -1,6 +1,5 @@
 ﻿using BlazorBootstrap;
 using DAL.Models;
-using Microsoft.AspNetCore.Identity;
 using System.Net.Http.Json;
 
 namespace CampingWebAssembly.Pages
@@ -8,6 +7,8 @@ namespace CampingWebAssembly.Pages
     public partial class Index
     {
         private string[] credentials { get; set; } = new string[2];
+
+        private string warningText = "";
 
         private Modal loginModal = default!;
 
@@ -18,6 +19,19 @@ namespace CampingWebAssembly.Pages
 
         private async Task Login()
         {
+            try
+            {
+                User? user = await http.GetFromJsonAsync<User>("api/User/" + credentials[0]);
+                if (user != null && user.Password.Equals(credentials[1]))
+                {
+                    LoggedUser = user;
+                    NavigationManager.Refresh();
+                }
+            }
+            catch
+            {
+                warningText = "Nombre de usuario o contraseña incorrectos";
+            }
         }
     }
 }
