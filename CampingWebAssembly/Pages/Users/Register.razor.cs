73f0aeaff996151213116cddf1f7c2df24c0abb2
Miguel Saez Perez <misape575@gmail.com>
2024-05-24 11:50:58 +0200

    Página de registro maomeno

---
+++ CampingWebAssembly/Pages/Users/Register.razor.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Models;
+using System.Net.Http.Json;
+
+namespace CampingWebAssembly.Pages.Users
+{
+    public partial class Register
+    {
+        //Usuario a crear y repetir contraseña
+        private User user { get; set; } = new();
+        private string pswrd = "";
+        private async Task CreateAccount()
+        {
+            if(user.Password.Equals(pswrd))
+            {
+                try
+                {
+                    await Http.PostAsJsonAsync("api/User", user);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
