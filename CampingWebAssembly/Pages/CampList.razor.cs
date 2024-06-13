490ead0074d3fbc596cdd208547071dceb5658f0
Miguel Saez Perez <misape575@gmail.com>
2024-06-13 12:06:19 +0200

    Lista llama a BD

--- CampingWebAssembly/Pages/CampList.razor.cs
+++ CampingWebAssembly/Pages/CampList.razor.cs
@@ -1,12 +1,27 @@
-﻿namespace CampingWebAssembly.Pages
+﻿using DAL.Models;
+using System.Net.Http.Json;
+
+namespace CampingWebAssembly.Pages
 {
     public partial class CampList
     {
         public bool showSearchMenu = false;
-
+		protected List<Camping> campings = new();
+		protected List<Camping> filteredCampings = new();
         public double priceValue = 0.00;
 
-        private void showSearch()
+		protected override async Task OnInitializedAsync()
+		{
+			await GetCampings();
+		}
+
+		protected async Task GetCampings()
+		{
+			campings = await Http.GetFromJsonAsync<List<Camping>>("api/Camping");
+			filteredCampings = campings;
+		}
+
+		private void showSearch()
         {
             showSearchMenu = !showSearchMenu;
         }
