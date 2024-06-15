b93fb78d7d92364494144861590310a902d08a74
Miguel Saez Perez <misape575@gmail.com>
2024-06-15 16:03:23 +0200

    Venatana de detalles

---
+++ CampingWebAssembly/Pages/Details.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using DAL.Models;
+using System.Net.Http.Json;
+
+namespace CampingWebAssembly.Pages
+{
+	public partial class Details
+	{
+		[Parameter]
+		public string? Id { get; set; }
+
+		public Camping? Camp { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			await GetCamping();
+		}
+
+		protected async Task GetCamping()
+		{
+			Camp = await Http.GetFromJsonAsync<Camping>("api/Camping/" + Id);
+		}
+	}
+}
