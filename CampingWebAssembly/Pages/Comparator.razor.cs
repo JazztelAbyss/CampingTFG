81cd33f0fb2b6aea2ee7991e1dff2c29e514a7d0
Miguel Saez Perez <misape575@gmail.com>
2024-06-18 16:30:08 +0200

    Ventana comparador

---
+++ CampingWebAssembly/Pages/Comparator.razor.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Models;
+using System.Net.Http.Json;
+
+namespace CampingWebAssembly.Pages
+{
+    public partial class Comparator
+    {
+        private List<Camping> campings { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetCampings();
+        }
+
+		protected async Task GetCampings()
+		{
+			campings = await Http.GetFromJsonAsync<List<Camping>>("api/Camping");
+		}
+	}
+}
