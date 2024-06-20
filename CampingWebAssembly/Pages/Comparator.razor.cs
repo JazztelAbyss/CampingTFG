1ef927f9f2e120e2574149528beeef860a0d7918
Miguel Saez Perez <misape575@gmail.com>
2024-06-20 16:39:53 +0200

    Inicio comparador

--- CampingWebAssembly/Pages/Comparator.razor.cs
+++ CampingWebAssembly/Pages/Comparator.razor.cs
@@ -1,4 +1,5 @@
-﻿using DAL.Models;
+﻿using BlazorBootstrap;
+using DAL.Models;
 using System.Net.Http.Json;
 
 namespace CampingWebAssembly.Pages
@@ -7,10 +8,17 @@ namespace CampingWebAssembly.Pages
     {
         private List<Camping> campings { get; set; } = new();
 
+        private Modal comparationModal = default!;
+
         protected override async Task OnInitializedAsync()
         {
             await GetCampings();
         }
+
+		private async Task OnShowModalClick()
+		{
+			await comparationModal.ShowAsync();
+		}
 
 		protected async Task GetCampings()
 		{
