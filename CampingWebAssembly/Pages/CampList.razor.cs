9e4004523ab28ad4f181bdac7a88e5798389bf4c
Miguel Saez Perez <misape575@gmail.com>
2024-06-21 15:54:59 +0200

    El mapa funciona + shenannigans

--- CampingWebAssembly/Pages/CampList.razor.cs
+++ CampingWebAssembly/Pages/CampList.razor.cs
@@ -10,6 +10,18 @@ namespace CampingWebAssembly.Pages
 		protected List<Camping> campings = new();
 		protected List<Camping> filteredCampings = new();
 
+		public class CampingCard
+		{
+			public string name { get; set; } = string.Empty;
+			public byte[] Image { get; set; } = Array.Empty<byte>();
+			public string Locality {  get; set; } = string.Empty;
+			public double price { get; set; }
+			public List<Service> Services { get; set; } = new();
+			public string CampingId { get; set; } = string.Empty;
+		}
+
+		public List<CampingCard> CampingCards {  get; set; } = new();
+
 		//Filtros de busqueda
 		string name = "";
 		IEnumerable<int> price = [25, 75];
@@ -19,12 +31,44 @@ namespace CampingWebAssembly.Pages
 		protected override async Task OnInitializedAsync()
 		{
 			await GetCampings();
+			await GetCards();
 		}
 
 		protected async Task GetCampings()
 		{
 			campings = await Http.GetFromJsonAsync<List<Camping>>("api/Camping");
 			filteredCampings = campings;
+		}
+
+		protected async Task GetCards()
+		{
+			foreach(var camping in campings)
+			{
+				CampingCards.Add(new CampingCard
+				{
+					name = camping.Name,
+					Image = camping.Portrait,
+					Locality = camping.Locality,
+					price = camping.Price,
+					Services = await GetServices(camping.Id),
+					CampingId = camping.Id
+				});
+			}
+		}
+
+		protected async Task<List<Service>> GetServices(string campingId)
+		{
+			var tags = await Http.GetFromJsonAsync<List<TagHolder>>("api/TagHolder/" + campingId);
+			List<Service> services = new List<Service>();
+			if(tags != null)
+			{
+				foreach(var tag in tags)
+				{
+					var service = await Http.GetFromJsonAsync<Service>("api/Service/" + tag.ServiceId.ToString());
+					if(service != null) {  services.Add(service); }
+				}
+			}
+			return services;
 		}
 
 		private void showSearch()
--- CampingWebAssembly/Pages/Map.razor.cs
+++ CampingWebAssembly/Pages/Map.razor.cs
@@ -1,10 +1,30 @@
-﻿using Radzen;
+﻿using DAL.Models;
+using Radzen;
 using Radzen.Blazor;
+using System.Net.Http.Json;
 
 namespace CampingWebAssembly.Pages
 {
 	public partial class Map
 	{
 		private RadzenGoogleMap map;
+
+		public List<Camping> Campings { get; set; } = new();
+		private Camping? SelectedCamping { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			await GetCampings();	
+		}
+
+		protected async Task GetCampings()
+		{
+			Campings = await Http.GetFromJsonAsync<List<Camping>>("api/Camping");
+		}
+
+		protected void OnMarkerClick(RadzenGoogleMapMarker marker)
+		{
+			SelectedCamping = Campings.Find(c => c.Id == marker.Title);
+		}
 	}
 }
